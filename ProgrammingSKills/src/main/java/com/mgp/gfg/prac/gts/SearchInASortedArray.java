6e642dacc58b55e5aea16971e6e9581c002dc252
mgpradeepa <mgpradeepa@gmail.com>
2021-11-05 16:27:02 +0530

    Few more programs for practice

--- ProgrammingSKills/src/main/java/com/mgp/gfg/prac/gts/SearchInASortedArray.java
+++ ProgrammingSKills/src/main/java/com/mgp/gfg/prac/gts/SearchInASortedArray.java
@@ -5,14 +5,10 @@ public class SearchInASortedArray {
         int N = 9;
         int[] A = {5, 6, 7, 8, 9, 10, 1, 2, 3};
         int key = 10;
-        Parihara parihara = new Parihara();
-        System.out.println(parihara.search(A, 0, N - 1, key));
+        System.out.println(search(A, 0, N - 1, key));
 
     }
-}
-
-class Parihara {
-    public int search(int[] A, int l, int h, int key) {
+    private static  int search(int[] A, int l, int h, int key) {
         // Complete this function
         // can complete in O(n)
         // to calculate in O(logN) can use binary search technique
