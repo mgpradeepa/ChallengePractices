dbfdde8590cfe720dad77b9d63e165a41a34a496
mgpradeepa <mgpradeepa@gmail.com>
2023-05-29 16:28:51 +0530

    LtC chal codes

---
+++ ProgrammingSKills/src/main/java/com/mgp/aoip/ISP/pracstring/lc/AddBinary.java
@@ -0,0 +1,14 @@
+package com.mgp.aoip.ISP.pracstring.lc;
+
+public class AddBinary {
+    public static void main(String[] args) {
+        String a = "11";
+        String b = "01";
+        Integer an = Integer.getInteger(a);
+        System.out.println(an);
+        Integer bn = Integer.getInteger(b);
+        System.out.println(bn);
+
+        System.out.println(an + bn );
+    }
+}
